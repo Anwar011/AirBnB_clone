72f1ad70eac133aba71777d7d2d2b965b5e047a0
Anwar011 <elmouharibi12@gmail.com>
2024-01-13 03:55:36 -0800

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,48 +1,79 @@
 #!/usr/bin/python3
 """
-
+base model of our airBnB
 """
-import uuid
+
 from datetime import datetime
+import models
+from uuid import uuid4
+
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+    """
+    Define BaseModel class
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initializes the `BaseModel` object.
+
+        Args:
+            *args: Additional positional arguments (if any).
+            **kwargs: Additional keyword arguments (if any).
+
+        Attributes:
+            id (str): The unique identifier of the object.
+            created_at (datetime): time of creation
+            updated_at (datetime): time of updates
+        """
+        if kwargs:
+            for key in kwargs:
+                if key == "__class__":
+                    pass
+                elif key == "id":
+                    self.id = kwargs[key]
+                elif key == "created_at":
+                    self.created_at = datetime.strptime(kwargs[key], "\
+%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.updated_at = datetime.strptime(kwargs[key], "\
+%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    setattr(self, key, kwargs[key])
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+
+    def __str__(self):
+        """
+        string method that return descriptor of the object
+        Return:
+            object descriptor
+        """
+        return ("[{}] ({}) {}\
+".format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
         """
-
+        save is a method that save time of updates and update
+        the updated_at instance attribute
         """
-        self.update_at = datetime.utcnow()
+        self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
-
+        to_dict is a method that get an prepared dict
+        Return:
+            new dictionary
         """
-        inst_dict = self.__dict__.copy()
-        inst_dict["__class__"] = self.__class__.__name__
-        inst_dict["created_at"] = self.created_at.isoformat()
-        inst_dict["updated_at"] = self.updated_at.isoformat()
+        dictionary = {}
+        for key in self.__dict__:
+            dictionary[key] = self.__dict__[key]
+        dictionary["__class__"] = self.__class__.__name__
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
+        return dictionary
 
-        return inst_dict
-    def __str__(self):
-        """
-
-        """
-        class_name = self.__class__.__name__
-        return "[{}]({}){}".format(class_name, self.id, self.__dict__)
-
-if __name__ == "__main__":
-    my_model = BaseModel()
-    my_model.name = "My First Model"
-    my_model.my_number = 89
-    print(my_model)
-    my_model.save()
-    print(my_model)
-    my_model_json = my_model.to_dict()
-    print(my_model_json)
-    print("JSON of my_model:")
-    for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
